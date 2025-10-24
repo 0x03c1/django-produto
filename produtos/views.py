28210a6cfde091c730dea417df12f3d02bf4e2f7
ronierisonmaciel <ronierison.maciel@gmail.com>
2025-10-24 17:25:03 -0300

    add initial models, views, admin, and configuration for produtos app

---
+++ produtos/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cadastro_produto():
+    # cadastro.html
+    pass
+
+def alterar_produto():
+    # alteracao.html
+    pass
+
+def exluir_produto():
+    # excluir.html
+    pass
